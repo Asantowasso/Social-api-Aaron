b5815dd76f0c2ac4b961ab8fca527d106a4e9d05
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-12-18 13:09:08 -0500

    adding routes and contollers

---
+++ controllers/userContoller.js
@@ -0,0 +1,8 @@
+// A route to GET all users
+// A route to GET all users by _id along with their thought and friend data
+// A route to POST a new user 
+// A PUT route to update a user by their ID
+// A DELETE route to remove a user by their ID
+// w/friendId a POST route to add a new friend to a user's friend list
+// w/friendId a DELETE route to remove a friend from a user's friend list
+
